ed79dd70c54c9b605521daeb6b8711333aa560d7
DeFiBreacher~ <defibreacher@gmail.com~>
2026-05-19 16:36:07 +0530

    feat: added post, added contact

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1779188766',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/audits/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
