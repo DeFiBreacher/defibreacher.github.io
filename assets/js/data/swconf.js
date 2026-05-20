34a2415c4146406fdcc3b2e5b2ee2af51c2dfb39
DeFiBreacher~ <defibreacher@gmail.com~>
2026-05-21 02:09:35 +0530

    feat: added new stats section

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1779193888',resources: [
+    cacheName: 'chirpy-1779309575',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
