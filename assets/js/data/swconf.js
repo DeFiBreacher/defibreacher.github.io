45ee1fba063b76d78cd77829e1d977b2b87325ed
DeFiBreacher~ <defibreacher@gmail.com~>
2026-05-21 06:17:27 +0530

    feat: improved home page

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1779310028',resources: [
+    cacheName: 'chirpy-1779324447',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
