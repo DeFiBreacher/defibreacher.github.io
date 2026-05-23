acea02d78edb4adfe9a2330cbf491711105eab22
DeFiBreacher~ <defibreacher@gmail.com~>
2026-05-23 18:30:51 +0530

    feat: added custom panel on homepage

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1779521435',resources: [
+    cacheName: 'chirpy-1779541250',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
