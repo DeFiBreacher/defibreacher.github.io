6a002baf50523f5ad495bbd6f673ec4f36386e9b
DeFiBreacher~ <defibreacher@gmail.com~>
2026-05-28 07:16:07 +0530

    feat: added new logo and css

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1779765701',resources: [
+    cacheName: 'chirpy-1779932766',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
