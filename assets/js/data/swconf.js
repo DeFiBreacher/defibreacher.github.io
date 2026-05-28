e6759d8154cd9b2da60a9c9f46f806278474d490
DeFiBreacher~ <defibreacher@gmail.com~>
2026-05-28 19:52:53 +0530

    feat: added a secret shhhh

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1779932766',resources: [
+    cacheName: 'chirpy-1779978173',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
