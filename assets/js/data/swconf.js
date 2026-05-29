64bd5ba657121b714d847d604eab6675be10e267
DeFiBreacher~ <defibreacher@gmail.com~>
2026-05-29 11:12:45 +0530

    feat: added a konami code

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1779978173',resources: [
+    cacheName: 'chirpy-1780033365',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
