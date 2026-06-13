65188a96090bcec6c5f280f65f4ec46b3d36dfd0
defibreacher <defibreacher@gmail.com>
2026-06-14 05:01:07 +0530

    feat: recreated Audit page #10

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1781393291',resources: [
+    cacheName: 'chirpy-1781393467',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
