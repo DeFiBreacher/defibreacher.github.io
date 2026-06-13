7c719968d3e6ab24dde183f4af901abc6d65097a
defibreacher <defibreacher@gmail.com>
2026-06-14 00:10:05 +0530

    feat: updated Audit page #5

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,34 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1781376005',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/audits/',
+      
+        '/reading/',
+      
+        '/ctf/',
+      
+        '/bucket-list/',
+      
+        '/terminal/',
+      
+        '/categories/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
