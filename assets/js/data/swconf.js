3314110aec50b5f1889585374a3a09a0a08e1e8c
defibreacher <defibreacher@gmail.com>
2026-06-19 00:56:54 +0530

    feat: finals tweaks#6

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1781789355',resources: [
+    cacheName: 'chirpy-1781810814',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
